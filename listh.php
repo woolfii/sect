52528b176a67d96cbe0202ffd2d549a6fa2b8343
juan ga <jcga4acb110@gmail.com>
2018-06-26 20:50:21 -0500

    listas

---
+++ listh.php
@@ -0,0 +1,19 @@
+<?php
+
+	include ("conexion.php");
+
+	$query = "SELECT * FROM herramientas";
+	$resultado = mysqli_query($conexion, $query);
+
+	if(!$resultado){
+		die("error");
+
+	}else{
+		while($data = mysqli_fetch_assoc($resultado)){
+			$arreglo["data"][] = $data;
+		}
+		echo json_encode($arreglo);
+	}
+	
+	mysqli_free_result($resultado);
+	mysqli_close($conexion);
